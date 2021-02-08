ca830a9cd9d147280dee57b1a585d6821874d52f
Nasrin-Rahimi <77.nasrin@gmail.com>
2021-02-08 08:02:48 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(string){
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+
+  static titleize(string){
+    let strArray = string.split(' ');
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    let result = [];
+
+    for(const word of strArray){
+      if(exceptions.includes(word)){
+        result.push(word);
+      }
+      else{
+        result.push(this.capitalize(word));
+      }
+    }
+    //Always capitilize the first word.
+    result[0] = this.capitalize(result[0]);
+    return result.join(' ');
+
+    // If we didn't have conditions,the below code does work.
+    // let result =  strArray.map(word => this.capitalize(word));
+  }
+
 }
