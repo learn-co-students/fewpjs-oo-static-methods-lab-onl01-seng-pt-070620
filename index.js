01b57ded0b0b72b1b73e99b56ea56555980922ac
mfioran1 <maurice97f@gmail.com>
2021-02-24 15:07:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,33 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^0-9a-z '-]/gi, '');
+  }
+
+  static titleize(string) {
+    const wrongWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+
+    let finalArray = [];
+
+    let newArray = string.split(' ')
+
+    for(let i=0; i < newArray.length; i++) {
+      if (i == 0) {
+        finalArray.push(this.capitalize(newArray[i]))
+      }
+      else {
+        if (wrongWords.includes(newArray[i])){
+          finalArray.push(newArray[i])
+        }
+        else {
+          finalArray.push(this.capitalize(newArray[i]))
+        }
+      }
+    }
+    return finalArray.join(' ')
+  }
 }
