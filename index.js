0110745d530735d0d5698c54f6189f278217c9ba
julieconnors <juliana.g.connors@gmail.com>
2021-05-17 14:59:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9 '-]/g, '');
+  }
+
+  static titleize(string) {
+    const avoid = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let words = string.split(" ")
+    let first = words.shift()
+    let firstUp = first.charAt(0).toUpperCase() + first.slice(1)
+
+    let rest = words.map(word => {
+      if (!avoid.includes(word)) {
+        return word.charAt(0).toUpperCase() + word.slice(1)
+      } else {
+        return word
+      }
+    })
+
+    return firstUp + " " + rest.join(" ")
+  }
 }
